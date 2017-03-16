68cbb0e786372890585ce2480c0381bafc9a1e74
justinhuynh <jhuynh@gmail.com>
2017-03-16 12:49:30 -0400

    initial commit

---
+++ react/test/GameSpec.js
@@ -0,0 +1,30 @@
+import '../src/Game'
+
+describe('Game', () => {
+  // Write tests here to test the Game Component with the full DOM.
+  // Don't forget to remove pending()!
+  beforeEach(() => {
+    jasmineEnzyme();
+    // add set up data as needed here
+  });
+
+  it('should have the specified inital state', () => {
+    pending();
+  });
+
+  it('should render an h3 (<h3>) with the title of the page', () => {
+    pending();
+  });
+
+  it('should render the GameTable', () => {
+    pending();
+  });
+
+  it('should render an X when a cell is clicked during X\'s turn', () => {
+    pending();
+  });
+
+  it("should update who's turn it is when a cell is clicked during X\'s turn", () => {
+    pending();
+  });
+});
---
+++ react/test/GameTableSpec.js
@@ -0,0 +1,26 @@
+import '../src/GameTable'
+
+describe('GameTable', () => {
+  // Write tests here for the GameTable Component.
+  // Don't forget to remove pending()!
+  beforeEach(() => {
+    jasmineEnzyme();
+    // add set up data as needed here
+  });
+
+  it('should render', () => {
+    pending();
+  });
+
+  it('should render an p tag containing a message with the current turn', () => {
+    pending();
+  });
+
+  it('should render a table with three rows (<tr>)', () => {
+    pending();
+  });
+
+  it('should render first row with three columns (<td>)', () => {
+    pending();
+  });
+});
